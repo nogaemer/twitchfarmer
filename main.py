8ce4eec60c6eb7597d94796364f393943a9d0718
Noah Schwenold <schwenoldnoah@gmail.com>
2024-05-02 18:49:42 +0200

    display Streamelements Points

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from TwitchChannelPointsMiner.classes.entities.Streamer import Streamer, Streame
 from keep_alive import keep_alive
 import os
 
-keep_alive()
+keep_alive(["lootraiider", "loot_treasure", "loot_bounty"])
 
 twitch_miner = TwitchChannelPointsMiner(
     username=os.environ.get('username'),
